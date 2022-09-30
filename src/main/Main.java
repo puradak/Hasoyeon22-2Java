104cbb6ce5b10f1883f0f54a5c731b3951b3580e
YJH <YJH@DESKTOP-4AC298Q>
2022-09-30 17:19:44 +0900

    1주차

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,18 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World~"); 
+		int num1=10;
+		int num2=40;
+		
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		
+		System.out.println("Hello" + " World");
+		
+		int year_value = 2022;
+		String year_key="년";
+		
+		System.out.println(year_value + year_key);
 	}
 }
