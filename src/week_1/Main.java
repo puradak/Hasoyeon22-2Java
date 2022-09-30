b168c8c316df762b34543c1893eddc96e4a02699
swoochoi <swoochoi@jrimchoi-pc>
2022-09-30 14:37:49 +0900

    week1 과제

---
+++ src/week_1/Main.java
@@ -0,0 +1,27 @@
+package week_1;
+
+public class Main {
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 40;
+		
+		System.out.println("num1 + num2 = " + num1 + num2 );
+		System.out.println("num1 - num2 = " + (num1 - num2) );
+		System.out.println("num1 * num2 = " + (num1 * num2) );
+		
+		System.out.println("Hello" + "World");
+		
+		int year_value = 2022;
+		String year_key = "년";
+		System.out.println(year_value + year_key);
+		
+	}
+
+}
