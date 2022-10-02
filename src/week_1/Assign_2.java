514065d40605775c40c87f2054a80aaf9b8349cf
YU JUYEONG <YU JUYEONG@LAPTOP-76MCOVVV>
2022-10-02 22:42:54 +0900

    과제2

---
+++ src/week_1/Assign_2.java
@@ -0,0 +1,16 @@
+package week_1;
+
+public class Assign_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a,b;
+		
+		a = "Hello";
+		b = "World";
+		
+		System.out.println((a)+(b));
+	}
+
+}
