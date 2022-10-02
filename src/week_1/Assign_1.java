293cb408e563b28989a85ab18358bddbf5b2a52a
YU JUYEONG <YU JUYEONG@LAPTOP-76MCOVVV>
2022-10-02 22:36:33 +0900

    과제1

---
+++ src/week_1/Assign_1.java
@@ -0,0 +1,19 @@
+package week_1;
+
+public class Assign_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1, num2, sum;
+		
+		num1 = 10;
+		num2 = 40;
+		
+		System.out.println("num1 + num2 = "+(num1+num2));
+		System.out.println("num1 - num2 = "+(num1-num2));
+		System.out.println("num1 * num2 = "+(num1*num2));
+
+	}
+
+}
