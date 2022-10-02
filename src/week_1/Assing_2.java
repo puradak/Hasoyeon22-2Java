c9cede9b42b682b239446f6908e57605c8c524d4
YU JUYEONG <YU JUYEONG@LAPTOP-76MCOVVV>
2022-10-02 22:40:04 +0900

    과제2

---
+++ src/week_1/Assing_2.java
@@ -0,0 +1,17 @@
+package week_1;
+
+public class Assing_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a,b;
+		
+		a = "Hello";
+		b = "World";
+		
+		System.out.println((a)+(b));
+
+	}
+
+}
