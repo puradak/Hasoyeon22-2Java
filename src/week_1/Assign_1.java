cb5b77d006e6d17ee84c362ac5f3fd024aa88635
박지수 <@192.168.10.113>
2022-10-02 22:27:31 +0900

    과제1

---
+++ src/week_1/Assign_1.java
@@ -0,0 +1,26 @@
+package week_1;
+
+public class Assign_1 {
+
+	public static void main(String[] args) {
+		// 1번
+		int num1 = 10, num2 = 40;
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		
+		// 2번
+		/*
+		String str1 = "Hello", str2 = " World";
+		System.out.println(str1 + str2);
+		 */
+		System.out.println("Hello" + " World");
+		
+		// 3번
+		int year_value = 2022;
+		String year_key = "년";
+		System.out.println(year_value + year_key);
+
+	}
+
+}
