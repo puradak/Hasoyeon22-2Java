0da73e2c4a00f7e63fb59fd528eb58ed732be5d1
YU JUYEONG <YU JUYEONG@LAPTOP-76MCOVVV>
2022-10-02 22:46:00 +0900

    과제3

---
+++ src/week_1/Assign_3.java
@@ -0,0 +1,18 @@
+package week_1;
+
+public class Assign_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int year_value;
+		String year_key;
+		
+		year_value = 2022;
+		year_key = "년";
+		
+		System.out.println((year_value)+(year_key));
+
+	}
+
+}
