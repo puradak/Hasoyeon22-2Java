4ff9c44951d255296e1e8a2c4687296e39cab226
ysm03 <ysm03@192.168.0.3>
2022-10-03 10:54:00 +0900

    자바 스터디 1주차 과제

---
+++ src/week_1/Assign_1.java
@@ -0,0 +1,23 @@
+package week_1;
+
+public class Assign_1 {
+
+	public static void main(String[] args) {
+		//1.
+		int num1 = 10, num2 = 40;
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		
+		//2.
+		System.out.println("Hello"+"Wolrd");
+		
+		//3.
+		int year_value;
+		String year_key;
+		year_value = 2022; year_key = "년";
+		System.out.println(year_value+year_key);
+
+	}
+
+}
