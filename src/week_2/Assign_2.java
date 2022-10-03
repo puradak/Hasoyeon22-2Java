bbfd393842b8a27d673a49cd835db7ad6c00a946
KimHeeYoung <KimHeeYoung@LAPTOP-9CNGUIG3>
2022-10-03 15:37:14 +0900

    week2 과제

---
+++ src/week_2/Assign_2.java
@@ -0,0 +1,10 @@
+package week_2;
+
+public class Assign_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello"+"World");
+
+	}
+
+}
---
+++ src/week_2/Assign_3.java
@@ -0,0 +1,13 @@
+package week_2;
+
+public class Assign_3 {
+
+	public static void main(String[] args) {
+		int year_value = 2022;
+		var year_key = "년";
+		
+		System.out.println(year_value + year_key);
+
+	}
+
+}
