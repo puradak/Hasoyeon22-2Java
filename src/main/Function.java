97af813f187ef40a0fcdd2d0f4f61ef8d16427f5
YJH <YJH@DESKTOP-4AC298Q>
2022-10-04 20:40:31 +0900

    week2

---
+++ src/main/Function.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Function {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,18 +2,8 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		int num1=10;
-		int num2=40;
-		
-		System.out.println(num1+num2);
-		System.out.println(num1-num2);
-		System.out.println(num1*num2);
-		
-		System.out.println("Hello" + " World");
-		
-		int year_value = 2022;
-		String year_key="년";
-		
-		System.out.println(year_value + year_key);
+		Function f=new Function();
+		int num = f.add(2, 3);
+		System.out.println(num);
 	}
 }
