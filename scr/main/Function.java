094b0f09a5be9397e98114a1125433170423bdb7
puradak <bbln2252@naver.com>
2022-10-04 20:40:30 +0900

    week2

---
+++ scr/main/Function.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Function {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	
+	
+}
--- scr/main/Main.java
+++ scr/main/Main.java
@@ -3,7 +3,12 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		
+		Function fun = new Function();
+		
+		int num = fun.add(2, 3);
+		
+		System.out.println(num);
 	}
 
 }
