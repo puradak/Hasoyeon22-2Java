939fef63964c21a26bca068f1c16adb2bbfc68a2
YJH <YJH@DESKTOP-4AC298Q>
2022-10-05 12:39:56 +0900

    week2과제

--- src/main/Function.java
+++ src/main/Function.java
@@ -1,8 +1,21 @@
 package main;
 
 public class Function {
-	
-	public int add(int a, int b) {
-		return a+b;
-	}
+		public int multi_sum(int[] arr) {
+			int result_odd=1;
+			int result_even=1;
+			
+			for(int i=0; i<arr.length; i++)
+			{
+				if(arr[i]%2==0)
+				{
+					result_even*=arr[i];
+				}
+				else
+				{
+					result_odd*=arr[i];
+				}
+			}
+			return result_odd+result_even;
+		}
 }
---
+++ src/main/Week2_assignment.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Week2_assignment {
+
+	public static void main(String[] args) {
+		int[] arr= {9,4,2,5,1};
+		
+		Function f=new Function();
+		int result=f.multi_sum(arr);
+		System.out.print(result);
+	}
+
+}
